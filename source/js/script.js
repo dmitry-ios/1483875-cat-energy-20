024671d2e77893861abecebd1dc4618bf1be7a4f
dmitry-ios <60504537+dmitry-ios@users.noreply.github.com>
2020-09-04 08:12:39 +0300

    Вносит правки по критериям и тз.

--- source/js/script.js
+++ source/js/script.js
@@ -50,3 +50,60 @@ if (afterButton) {
     }
   });
 }
+
+var form = document.querySelector(".page-main__form");
+var submitButton = document.querySelector("[type=submit]");
+
+if (form) {
+  submitButton.addEventListener("click", function(event) {
+    var name = document.querySelector("[name=name]");
+    var weight = document.querySelector("[name=weight]");
+    var email = document.querySelector("[name=email]");
+    var emailIcon = document.querySelector(".owner-contacts__item--mail .owner-contacts__icon")
+    var phone = document.querySelector("[name=phone]");
+    var phoneIcon = document.querySelector(".owner-contacts__item--phone .owner-contacts__icon");
+    var hasFocus = false;
+
+    name.classList.remove("page-main__form-input--status-error");
+    weight.classList.remove("page-main__form-input--status-error");
+    email.classList.remove("page-main__form-input--status-error");
+    emailIcon.classList.remove("owner-contacts__icon--status-error");
+    phone.classList.remove("page-main__form-input--status-error");
+    phoneIcon.classList.remove("owner-contacts__icon--status-error");
+
+    if(!name.value || !weight.value || !email.value || !phone.value) {
+      event.preventDefault();
+
+      if (!name.value) {
+        name.classList.add("page-main__form-input--status-error");
+        name.focus();
+        hasFocus = true;
+      }
+
+      if (!weight.value) {
+        weight.classList.add("page-main__form-input--status-error");
+        if (!hasFocus) {
+          weight.focus();
+          hasFocus = true;
+        }
+      }
+
+      if (!email.value) {
+        email.classList.add("page-main__form-input--status-error");
+        emailIcon.classList.add("owner-contacts__icon--status-error");
+        if (!hasFocus) {
+          email.focus();
+          hasFocus = true;
+        }
+      }
+
+      if (!phone.value) {
+        phone.classList.add("page-main__form-input--status-error");
+        phoneIcon.classList.add("owner-contacts__icon--status-error");
+        if (!hasFocus) {
+          phone.focus();
+        }
+      }
+    }
+  });
+}
